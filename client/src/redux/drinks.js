708dfc6f75165c3247b345b52c6d005ffd6eca29
Allison Shaw <shaw.allison91@gmail.com>
2018-05-12 11:03:42 -0600

    changed redux and drinks model

--- client/src/redux/drinks.js
+++ client/src/redux/drinks.js
@@ -1,48 +1,114 @@
 import axios from "axios";
+const url = "/drinks/";
 
-const initialState = {
-  data: [],
-  loading: true,
-  errMsg: ""
-}
-
-const drinksReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "GET_RANDOM":
-      return {
-        ...state,
-        data: action.random,
-        loading: false
-      }
-    case "ERR_MSG":
-      return {
-        ...state,
-        loading: false,
-        errMsg: action.errMsg
-      }
-    default: 
-      return state;
-  }
-}
-
-const drinkUrl = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
-
-export const getDrink = () => {
+export function getDrinks() {
   return dispatch => {
-    axios.get(drinkUrl)
+    axios
+      .get(url)
       .then(response => {
         console.log(response.data);
         dispatch({
-          type: "GET_DRINK",
-          random: response.data.drinks
-        })
+          type: "GET_DRINKS",
+          issue: response.data
+        });
       })
       .catch(err => {
+        console.error(err);
+      });
+  };
+}
+
+export function addDrink(newDrink) {
+  return dispatch => {
+    axios
+      .post(url, newDrink)
+      .then(response => {
+        console.log(response);
+        let { data } = response;
         dispatch({
-          type: "ERR_MSG",
-          errMsg: "Cannot get drinks."
-        })
+          type: "ADD_DRINK",
+          data
+        });
       })
+      .catch(err => {
+        console.error(err);
+      });
+  };
+}
+
+export function editDrink(editedDrink, id) {
+  return dispatch => {
+    console.log(editedDrink);
+    axios
+      .put(url + id, editedDrink)
+      .then(response => {
+        console.log(response);
+        dispatch({
+          type: "EDIT_DRINK",
+          editedDrink: response.data,
+          id
+        });
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
+}
+
+export function deleteDrink(id) {
+  return dispatch => {
+    axios
+      .delete(url + id, id)
+      .then(response => {
+        dispatch({
+          type: "DELETE_DRINK",
+          id
+        });
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
+}
+
+export default function drinksReducer(
+  prevState = { data: [], loading: true },
+  action
+) {
+  switch (action.type) {
+    case "GET_DRINKS":
+      return {
+        data: action.issue,
+        loading: false
+      };
+
+    case "ADD_DRINK":
+      return {
+        data: [...prevState.data, action.data],
+        loading: false
+      };
+
+    case "EDIT_DRINK":
+      return {
+        data: prevState.data.map(drink => {
+          if (drink._id === action.id) {
+            return action.editedDrink;
+          } else {
+            return drink;
+          }
+        }),
+        loading: false
+      };
+
+    case "DELETE_DRINK":
+      return {
+        data: prevState.data.filter(drink => {
+          return drink._id !== action.id;
+        }),
+        loading: false
+      };
+
+    default:
+      return prevState;
   }
 }
-export default drinkReducer;
--- client/src/redux/search.js
+++ client/src/redux/search.js
@@ -1,47 +1,47 @@
-import axios from "axios";
+// import axios from "axios";
 
-const initialState = {
-  data: [],
-  loading: true,
-  errMsg: ""
-}
-const searchReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "GET_INGREDIENT":
-      return {
-        ...state,
-        data: action.ingredient,
-        loading: false
-      }
-    case "ERR_MSG":
-      return {
-        ...state,
-        loading: false,
-        errMsg: action.errMsg
-      }
-    default:
-      return state
-  }
-}
+// const initialState = {
+//   data: [],
+//   loading: true,
+//   errMsg: ""
+// }
+// const searchReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case "GET_INGREDIENT":
+//       return {
+//         ...state,
+//         data: action.ingredient,
+//         loading: false
+//       }
+//     case "ERR_MSG":
+//       return {
+//         ...state,
+//         loading: false,
+//         errMsg: action.errMsg
+//       }
+//     default:
+//       return state
+//   }
+// }
 
-const searchUrl = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i={input}";
+// const searchUrl = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i={input}";
 
-export const getIngredient = () => {
-  return dispatch => {
-    axios.get(searchUrl)
-      .then(response => {
-        console.log(response.data);
-        dispatch({
-          type: "GET_INGREDIENT",
-          drinks: response.data.drinks
-        })
-      })
-      .catch(err => {
-        dispatch({
-          type: "ERR_MSG",
-          errMsg: "Sorry, data is unavalaible."
-        })
-      })
-  }
-}
-export default searchReducer;
+// export const getIngredient = () => {
+//   return dispatch => {
+//     axios.get(searchUrl)
+//       .then(response => {
+//         console.log(response.data);
+//         dispatch({
+//           type: "GET_INGREDIENT",
+//           drinks: response.data.drinks
+//         })
+//       })
+//       .catch(err => {
+//         dispatch({
+//           type: "ERR_MSG",
+//           errMsg: "Sorry, data is unavalaible."
+//         })
+//       })
+//   }
+// }
+// export default searchReducer;
